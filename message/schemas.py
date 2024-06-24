d5dab28fefc926c34ee904de17b2fb9e3fd2f6fa
sreekari <sreekari.p@gmail.com>
2024-06-24 15:04:13 +0530

    add schemas.py file to message folder

--- message/schemas.py
+++ message/schemas.py
@@ -1,22 +1,23 @@
 from pydantic import BaseModel
 from uuid import UUID
+from datetime import datetime
 
 class MessageBase(BaseModel):
     thread_id: UUID
     role: str
+    content: str
 
 class MessageCreate(MessageBase):
     pass
 
-class MessageInDBBase(MessageBase):
+class MessageUpdate(MessageBase):
+    pass
+
+class Message(MessageBase):
     id: UUID
     is_active: bool
+    created_at: datetime
+    updated_at: datetime
 
     class Config:
         orm_mode = True
-
-class Message(MessageInDBBase):
-    pass
-
-class MessageInDB(MessageInDBBase):
-    pass
