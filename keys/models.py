1f99fe42c89ae20dda50b5c0f72511774e5397fc
sreekari <sreekari.p@gmail.com>
2024-06-24 13:06:32 +0530

    add models.py file to key folder

--- keys/models.py
+++ keys/models.py
@@ -1,15 +1,12 @@
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from common import CommonBase
-from databases import Base
+from common import Base
 from sqlalchemy.dialects.postgresql import UUID
 
-class Key(CommonBase, Base):
-    __tablename__ = 'keys'
-    
-    anthropic_apikey = Column(String, nullable=False)
-    openai_apikey = Column(String, nullable=False)
-    mistral_apikey = Column(String, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), unique=True)
-    
+class Key(Base):
+    __tablename__ = "keys"
+    anthropic_apikey = Column(String)
+    openai_apikey = Column(String)
+    mistral_apikey = Column(String)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
     user = relationship("User", back_populates="key")
