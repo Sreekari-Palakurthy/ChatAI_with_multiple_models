44e66594398b62770c4b9f0ef648074f44f556d2
sreekari <sreekari.p@gmail.com>
2024-06-24 13:17:23 +0530

    add models.py file to message folder

--- message/models.py
+++ message/models.py
@@ -6,5 +6,5 @@ from sqlalchemy.dialects.postgresql import UUID
 class Message(Base):
     __tablename__ = "messages"
     thread_id = Column(UUID(as_uuid=True), ForeignKey("threads.id"))
-    role = Column(String)  # Either 'user' or 'assistant'
+    role = Column(String)
     thread = relationship("Thread", back_populates="messages")
