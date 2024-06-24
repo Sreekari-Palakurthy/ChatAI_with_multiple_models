6f0838787db94efe5f854556a018ae6a59b3a56d
sreekari <sreekari.p@gmail.com>
2024-06-24 16:22:49 +0530

    add models.py file to user folder

--- user/models.py
+++ user/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String,Integer
 from sqlalchemy.orm import relationship
 from common import Base
 
@@ -6,7 +6,7 @@ class User(Base):
     __tablename__ = "users"
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
-    phone_number = Column(String, unique=True, index=True)
+    phone_number = Column(Integer, unique=True, index=True)
     password = Column(String)
     threads = relationship("Thread", back_populates="owner")
     key = relationship("Key", back_populates="user", uselist=False)
