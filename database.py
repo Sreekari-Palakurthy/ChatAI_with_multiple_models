81b2d46ee82eb2d24bc8f18c00f081f2f86db4d1
sreekari <sreekari.p@gmail.com>
2024-06-24 13:00:50 +0530

    add database.py file

--- database.py
+++ database.py
@@ -1,18 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-
-async def init_db():
-    async with engine.begin() as conn:
-        from user.models import User
-        from keys.models import Key
-        from thread.models import Thread
-        from message.models import Message
-        await conn.run_sync(User.metadata.create_all)
-        await conn.run_sync(Key.metadata.create_all)
-        await conn.run_sync(Thread.metadata.create_all)
-        await conn.run_sync(Message.metadata.create_all)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
