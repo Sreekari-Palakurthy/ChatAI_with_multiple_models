1074fba3d38c3e2e3b28fcc702cc0c91197dfa98
sreekari <sreekari.p@gmail.com>
2024-06-24 15:14:36 +0530

    add views.py to user folder

--- user/views.py
+++ user/views.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from user import crud, schemas
 from uuid import UUID
+from typing import List
 
 router = APIRouter()
 
-
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -27,3 +28,17 @@ def read_user(user_id: UUID, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+@router.patch("/users/{user_id}", response_model=schemas.User)
+def update_user(user_id: UUID, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.update_user(db=db, user=db_user, user_update=user_update)
+
+@router.delete("/users/{user_id}", response_model=schemas.User)
+def soft_delete_user(user_id: UUID, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return crud.soft_delete_user(db=db, user_id=user_id)
