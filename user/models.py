19af474ab1df14e5a461fea7a73b734cf47f8a90
sreekari <sreekari.p@gmail.com>
2024-06-24 11:35:01 +0530

    add user folder

---
+++ user/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    
+    keys = relationship("Key", back_populates="user")
+    threads = relationship("Thread", back_populates="creator")
+    messages = relationship("Message", back_populates="sender")
