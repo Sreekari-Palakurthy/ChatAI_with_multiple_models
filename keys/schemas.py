26f3d064626eb311633a29ecbd8a4a399e5700e9
sreekari <sreekari.p@gmail.com>
2024-06-24 13:07:46 +0530

    add schemas.py file to keys folder

--- keys/schemas.py
+++ keys/schemas.py
@@ -1,21 +1,30 @@
 from pydantic import BaseModel
+from typing import Optional
 from uuid import UUID
-from datetime import datetime
 
 class KeyBase(BaseModel):
+    anthropic_apikey: Optional[str] = None
+    openai_apikey: Optional[str] = None
+    mistral_apikey: Optional[str] = None
+
+class KeyCreate(KeyBase):
     anthropic_apikey: str
     openai_apikey: str
     mistral_apikey: str
 
-class KeyCreate(KeyBase):
-    user_id: UUID
+class KeyUpdate(KeyBase):
+    pass
 
-class Key(KeyBase):
-    key_id: UUID
-    user_id: UUID
+class KeyInDBBase(KeyBase):
+    id: UUID
     is_active: bool
-    created_at: datetime
-    updated_at: datetime
+    user_id: UUID
 
     class Config:
         orm_mode = True
+
+class Key(KeyInDBBase):
+    pass
+
+class KeyInDB(KeyInDBBase):
+    pass
