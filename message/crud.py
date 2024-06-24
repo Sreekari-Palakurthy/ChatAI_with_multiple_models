53a133ba78bf3b1e7cb222f56042e277d80cf9fa
sreekari <sreekari.p@gmail.com>
2024-06-24 14:21:31 +0530

    add crud.py file to message folder

--- message/crud.py
+++ message/crud.py
@@ -16,6 +16,17 @@ def create_message(db: Session, message: MessageCreate):
     db.refresh(db_message)
     return db_message
 
+def get_all_thread_messages(db: Session, thread_id: UUID):
+    return db.query(Message).filter(Message.thread_id == thread_id).all()
+
+def update_message(db: Session, message: Message, message_update: MessageUpdate):
+    for key, value in message_update.dict(exclude_unset=True).items():
+        setattr(message, key, value)
+    db.commit()
+    db.refresh(message)
+    return message
+
+
 def delete_message(db: Session, message_id: UUID):
     message = db.query(Message).filter(Message.id == message_id).first()
     db.delete(message)
