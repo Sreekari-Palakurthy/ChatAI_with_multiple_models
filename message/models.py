bf6492eb8fbae2cf9c1c85b51fd36f562d41164c
sreekari <sreekari.p@gmail.com>
2024-06-24 13:16:57 +0530

    add models.py file to message folder

--- message/models.py
+++ message/models.py
@@ -1,18 +1,10 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime,func
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from common import Base
 from sqlalchemy.dialects.postgresql import UUID
-import uuid
-
-Base = declarative_base()
 
 class Message(Base):
-    __tablename__ = 'messages'
-    message_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    thread_id = Column(UUID(as_uuid=True), ForeignKey('threads.thread_id'))
-    user_id = Column(UUID(as_uuid=True), ForeignKey('users.user_id'))
-    content = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=func.now())
-    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    __tablename__ = "messages"
+    thread_id = Column(UUID(as_uuid=True), ForeignKey("threads.id"))
+    role = Column(String)  # Either 'user' or 'assistant'
     thread = relationship("Thread", back_populates="messages")
-    sender = relationship("User", back_populates="messages")
