78eba4b90ead4670d4fe016c66cd92c5ba9e8517
sreekari <sreekari.p@gmail.com>
2024-06-25 12:05:27 +0530

    add models.py file to user folder

--- user/models.py
+++ user/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String,Integer
 from sqlalchemy.orm import relationship
-from common import Base
+from common.models import Base
 
 class User(Base):
     __tablename__ = "users"
