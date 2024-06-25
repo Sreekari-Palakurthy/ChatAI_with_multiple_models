1b300675d13c760056ee1e36354cc7ba61d74b35
sreekari <sreekari.p@gmail.com>
2024-06-25 13:40:02 +0530

    add views.py file to user folder

--- user/views.py
+++ user/views.py
@@ -6,12 +6,11 @@ from uuid import UUID
 from common.connection import get_db
 from typing import List
 from user.models import User
-from api.openai_api import get_openai_models
-from api.anthropic_api import validate_anthropic_key
-from api.mistral_api import get_mistral_models
+from user.helpers import get_openai_models
+from user.helpers import validate_anthropic_key
+from user.helpers import get_mistral_models
 
 router = APIRouter()
-
 
 @router.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
@@ -41,22 +40,11 @@ def soft_delete_user(user_id: UUID, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return crud.soft_delete_user(db=db, user_id=user_id)
 
-@router.get("/users/models/openai", response_model=List[str])
-def get_openai_models_view():
-    return get_openai_models()
-
-@router.get("/users/models/anthropic", response_model=bool)
-def validate_anthropic_key_view():
-    return validate_anthropic_key()
-
-@router.get("/users/models/mistral", response_model=List[str])
-def get_mistral_models_view():
-    return get_mistral_models()
 
 @router.get("/users/models/", response_model=dict)
 def get_all_models():
     return {
         "openai_models": get_openai_models(),
         "mistral_models": get_mistral_models(),
-        "anthropic_models": validate_anthropic_key_view()
+        "anthropic_models": validate_anthropic_key()
     }
