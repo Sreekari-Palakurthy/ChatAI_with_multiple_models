4c2126fb4a10892376e2736bad8912f078bd22b0
sreekari <sreekari.p@gmail.com>
2024-06-25 10:43:03 +0530

    add views.py file to keys folder

--- keys/views.py
+++ keys/views.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from keys import crud, schemas
 from uuid import UUID
+from common_app.dependencies import get_db
 
 
 router = APIRouter()
